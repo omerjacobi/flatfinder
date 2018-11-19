7bae56c020ec29d92c0a131cf096df22a8b3a50e
omer.jacobi <omer.jacobi@gmail.com>
2018-11-19 15:44:40 +0200

    Initial commit

---
+++ app/src/main/java/com/huji/cse/flatfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huji.cse.flatfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
