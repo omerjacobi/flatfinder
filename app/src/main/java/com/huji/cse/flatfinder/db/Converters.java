a83094d6648bd8f7ec894f2d380b3b8cbc44532b
omer.jacobi <omer.jacobi@gmail.com>
2019-01-08 12:04:40 +0200

    update sql filter query

--- app/src/main/java/com/huji/cse/flatfinder/db/Converters.java
+++ app/src/main/java/com/huji/cse/flatfinder/db/Converters.java
@@ -8,7 +8,7 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-class Converters {
+public class Converters {
     @TypeConverter
     public static ArrayList<String> fromString(String value) {
         Type listType = new TypeToken<ArrayList<String>>() {}.getType();
--- app/src/main/java/com/huji/cse/flatfinder/db/dao/PostDao.java
+++ app/src/main/java/com/huji/cse/flatfinder/db/dao/PostDao.java
@@ -45,7 +45,7 @@ public interface PostDao {
     @Query("SELECT * FROM post_database WHERE number_of_roommates < :maxRoommates")
     LiveData<List<FacebookPost>>  getAllPostWithLessRoommates(long maxRoommates);
     @Query("SELECT * FROM post_database WHERE (((NOT :filterDistance) OR ((gps_lat BETWEEN :minLat AND :maxLat) AND  (gps_long" +
-            " BETWEEN :minLong AND :maxLong))) AND (number_of_roommates < :maxRoommates AND flat_price < :maxPrice) AND ((NOT :onlyFavorites) OR favorite_post))")
+            " BETWEEN :minLong AND :maxLong))) AND (number_of_roommates <= :maxRoommates AND flat_price <= :maxPrice) AND ((NOT :onlyFavorites) OR favorite_post))")
     LiveData<List<FacebookPost>>  getPostAfterFilter(double minLat, double maxLat, double minLong,
                                                      double maxLong, long maxPrice, long maxRoommates, boolean filterDistance, boolean onlyFavorites);
 
