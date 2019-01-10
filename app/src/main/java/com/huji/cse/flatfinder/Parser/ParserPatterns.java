9cc65a2535be8e225ebd128dc00d83998a72a664
yael-renous <yael.renous@cs.huji.ac.il>
2019-01-10 12:56:36 +0200

    changed regex for address - includes ','

--- app/src/main/java/com/huji/cse/flatfinder/Parser/ParserPatterns.java
+++ app/src/main/java/com/huji/cse/flatfinder/Parser/ParserPatterns.java
@@ -11,6 +11,6 @@ class ParserPatterns {
     static final Pattern addressPattern = Pattern.compile("(?:a|A)ddress");
     static final Pattern noRoommatesPattern = Pattern.compile("(?:of)* *(?:r|R)oom(?:m)?ates");
     static final Pattern numberPattern = Pattern.compile("\\d+");
-    static final Pattern stringPattern = Pattern.compile("((?:\\w+ *)+),*:*-*\\\\*(?:\\n)*");
+    static final Pattern stringPattern = Pattern.compile("((?:\\w+ *)+):*-*\\\\*(?:\\n)*\\.*");
     static final Pattern namePattern = Pattern.compile("Photos from (.+)'s post");
 }
